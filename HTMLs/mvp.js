ba71cd20fa9db0741c1fb90fcdf4fb3ffc0479d1
Georg Richter <georg.richter@stud.htw-dresden.de>
2025-05-08 23:49:29 +0200

    QuizServer-Schnittstelle hinzugefügt

--- HTMLs/mvp.js
+++ HTMLs/mvp.js
@@ -1,11 +1,33 @@
 "use strict";
 
+let quizData = {
+    it: [
+        {
+            frage: "Was bedeutet HTML?",
+            answers: ["HyperText Markup Language", "Home Tool Markup Language", "Hyperlinks and Text Markup Language", "Hyper Tool Multi Language"],
+        },
+        {
+            frage: "Was macht CSS?",
+            answers: ["Farbe und Layout", "Struktur", "Programmiert Logik", "Verwaltet Datenbanken"],
+        }
+    ]
+    // weitere Kategorien später möglich
+};
+
 document.addEventListener('DOMContentLoaded', function () {
     let m = new Model();
     let p = new Presenter();
     let v = new View(p);
     p.setModelAndView(m, v);
 });
+
+function getXhr() { // API für asynchrone Aufrufe
+    if (window.XMLHttpRequest) {
+      var xhr = new XMLHttpRequest();
+      return xhr;
+    } 
+    else return false;
+}
 
 // ################ Model ###########################
 class Model {
@@ -24,26 +46,68 @@ class Model {
         };
         this.currentIndex = 0;
         this.correctAnswers = 0;
+        this.questdata = {};
+        this.answer = "";
+        this.xhrreq = false;
+        this.isCorrect = false; 
+        this.quiznr = 5;
+        this.quizlen = 0;
+
+        const username=  "richter_georg@outlook.com";
+        const pw = "secret";
+        this.userdata = btoa(username + ":" + pw); //Base-64
+        this.xhr = getXhr();
     }
 
-    getTask() {
-        if (this.currentIndex < this.quizData.it.length) {
-            return this.quizData.it[this.currentIndex];
+    setCategory(cat) {
+        if (this.quizData[cat]) {
+            this.currentCategory = cat;
+            this.currentIndex = 0;
+            this.correctAnswers = 0;
+            if(cat = "it") {
+                this.quiznr = 1595
+                this.quizLen = 10;
+            }
+            if(cat = "math"){
+                this.quiznr = 1595;
+            }
+            if(cat == "piano"){
+                this.quiznr = 1595;
+            }
         }
-        return null;
     }
 
-    checkAnswer(answer) {
-        let correct = this.quizData.it[this.currentIndex].answers[0];
-        let isCorrect = (answer === correct);
-        if (isCorrect) this.correctAnswers++;
-        this.currentIndex++;
-        return isCorrect;
+    getTask(callback) {
+        if (this.currentIndex < this.quizLen) {
+            let task= this.sendQstXhr(callback,this.quiznr+this.currentIndex);
+        }else 
+            callback(null);
+    }
+
+    checkAnswer(callback) {
+        if(this.xhrreq == true)
+        {
+            this.xhrreq = false;
+            let i=0;
+            for(i=0; i< 4; i++) //4: Anzahl der Options
+                if(this.answer === this.questdata.options[i])
+                    break;
+            this.sendAnsXhr(callback, this.questdata.id+this.currentIndex -1 , i);
+
+        }
+        else{
+            let correct = this.quizData.it[this.currentIndex].answers[0];
+            if (this.answer === correct) {
+                this.correctAnswers++;
+                return true;
+            }
+            return false;
+        }
     }
 
     getProgress() {
         return {
-            total: this.quizData.it.length,
+            total: this.quizLen,
             current: this.currentIndex,
             correct: this.correctAnswers
         };
@@ -52,6 +116,56 @@ class Model {
     reset() {
         this.currentIndex = 0;
         this.correctAnswers = 0;
+    }
+
+    
+    sendQstXhr(callback,nr) {
+        this.xhr = getXhr();
+        this.xhrreq = true;
+        this.xhr.onreadystatechange = () => {
+            if (this.xhr.readyState !== 4) return;
+
+            if (this.xhr.status === 200) {
+                try{
+                    this.questdata = JSON.parse(this.xhr.responseText);
+                    console.log("Daten geladen:", this.questdata);
+                    callback(true);  // Text ist Ergebnis
+                }catch(e){
+                    console.error("Fehler beim Parsen der Daten", e);
+                    callback(false);
+                }
+            } else {
+                callback("Fehler beim Laden der Aufgabe.");
+            }
+        };
+        this.xhr.open('GET', 'https://idefix.informatik.htw-dresden.de:8888/api/quizzes/' + nr);
+        this.xhr.setRequestHeader("Authorization", "Basic " + this.userdata);
+        this.xhr.send(null);
+    }
+
+    sendAnsXhr(callback,nr, index) {
+        this.xhr = getXhr();
+        this.xhr.onreadystatechange = () => {
+            if (this.xhr.readyState !== 4)
+                return;
+
+            if (this.xhr.status === 200) {
+                try{
+                    this.questdata= JSON.parse(this.xhr.responseText);
+                    console.log("Daten erhalten:", this.questdata);
+                    callback(true);  // Text ist Ergebnis
+                }catch(e){
+                    console.error("Fehler beim Parsen der Daten", e);
+                    callback(false);
+                }
+            } else {
+                callback("Fehler beim Laden der Lösung.");
+            }
+        };
+        this.xhr.open('POST', 'https://idefix.informatik.htw-dresden.de:8888/api/quizzes/'+nr+'/solve');
+        this.xhr.setRequestHeader("Authorization", "Basic " + this.userdata);
+        this.xhr.setRequestHeader("Content-Type", "application/json");
+        this.xhr.send("["+index+"]");
     }
 }
 
@@ -71,30 +185,46 @@ class Presenter {
         View.renderErgebnis(`Du hast ${stats.correct} von ${stats.total} Fragen richtig beantwortet.`);
     }
 
-    setTask() {
-        let frag = this.m.getTask();
-        if (frag) {
-            View.renderText(frag.frage);
-            let shuffled = [...frag.answers].sort(() => Math.random() - 0.5);
-            shuffled.forEach((ans, i) => {
-                View.inscribeButtons(i, ans, ans);  // pos = tatsächlicher Antworttext
-            });
-        } else {
+   setTask() {
+        //let frag = await this.m.getTask();
+        this.m.getTask(() => {
+            let frag = this.m.questdata;
+            View.renderText(frag.text);
+            if (frag && frag.text && frag.options) {
+                View.renderText(frag.text);
+                let shuffled = [...frag.options].sort(() => Math.random() - 0.5);
+                this.m.currentIndex++;
+                shuffled.forEach((ans, i) => {
+                    View.inscribeButtons(i, ans, ans);  // pos = tatsächlicher Antworttext
+                });
+            } else {
 
-            View.renderText("Quiz beendet!");
-            this.showStats();
-            View.disableButtons();
-            this.m.reset();
-            document.getElementById("next").textContent = "Nochmal Fragen lösen";
-        }
-        this.updateProgressBar();
+                View.renderText("Quiz beendet!");
+                this.showStats();
+                View.disableButtons();
+                this.m.reset();
+                document.getElementById("next").textContent = "Nochmal Fragen lösen";
+            }
+            this.updateProgressBar();
+        });
     }
 
     checkAnswer(answerText) {
-        const isCorrect = this.m.checkAnswer(answerText);
-        View.renderErgebnis(isCorrect ? "✅ Richtig!" : "❌ Falsch!");
-        View.disableButtons();
-        this.updateProgressBar();
+        this.m.answer = answerText;
+        this.m.checkAnswer(() => {
+            let antw = this.m.questdata;
+            if(antw){
+                if(antw.success){
+                    this.m.correctAnswers++;
+                    View.renderErgebnis("✅ '"+answerText+"' ist Richtig!");
+                }
+                else
+                    View.renderErgebnis("❌ '"+answerText+"' ist Falsch!");
+                View.disableButtons();
+                this.updateProgressBar();
+            }
+            else View.renderErgebnis("Fehler");
+        });
     }
 
     updateProgressBar() {
@@ -116,14 +246,15 @@ class View {
 
     setHandler() {
         document.getElementById("answers").addEventListener("click", this.checkEvent.bind(this), false);
-        document.getElementById("it").addEventListener("click", this.start.bind(this), false);
+        document.getElementById("it").addEventListener("click", this.startit.bind(this), false);
         document.getElementById("next").addEventListener("click", this.loadNext.bind(this), false)
         document.getElementById("cancel").addEventListener("click", this.cancelQuiz.bind(this), false)
     }   
 
 
-    start() {
+    startit() {
         document.getElementById("next").disabled = false;
+        this.p.m.setCategory("it");
         this.loadNext();
     }
 
@@ -137,6 +268,7 @@ class View {
         View.renderText("Quiz abgebrochen");
         this.p.showStats();
         this.p.m.reset();
+        this.p.updateProgressBar();
         document.getElementById("next").textContent = "Nochmal Fragen lösen";
         View.disableButtons();
 
