923d73289eb2488f80314f5a80384302e6de0f9c
Georg Richter <georg.richter@stud.htw-dresden.de>
2025-06-07 12:36:48 +0200

    unnötige Pfade entfernt

--- src/service-worker.js
+++ src/service-worker.js
@@ -10,34 +10,6 @@ const filesToCache = [
     "manifest.webmanifest",
     
     "Images/birne.png"
-    /*
-    "scripts/katex/katex.min.js",
-    "scripts/katex/katex.min.css",
-    "scripts/katex/contrib/auto-render.min.js",
-    // KaTeX fonts (nur .woff2 für Kompaktheit)
-    "scripts/katex/fonts/KaTeX_AMS-Regular.woff2",
-    "scripts/katex/fonts/KaTeX_Caligraphic-Bold.woff2",
-    "scripts/katex/fonts/KaTeX_Caligraphic-Regular.woff2",
-    "scripts/katex/fonts/KaTeX_Fraktur-Bold.woff2",
-    "scripts/katex/fonts/KaTeX_Fraktur-Regular.woff2",
-    "scripts/katex/fonts/KaTeX_Main-Bold.woff2",
-    "scripts/katex/fonts/KaTeX_Main-BoldItalic.woff2",
-    "scripts/katex/fonts/KaTeX_Main-Italic.woff2",
-    "scripts/katex/fonts/KaTeX_Main-Regular.woff2",
-    "scripts/katex/fonts/KaTeX_Math-BoldItalic.woff2",
-    "scripts/katex/fonts/KaTeX_Math-Italic.woff2",
-    "scripts/katex/fonts/KaTeX_SansSerif-Bold.woff2",
-    "scripts/katex/fonts/KaTeX_SansSerif-Italic.woff2",
-    "scripts/katex/fonts/KaTeX_SansSerif-Regular.woff2",
-    "scripts/katex/fonts/KaTeX_Script-Regular.woff2",
-    "scripts/katex/fonts/KaTeX_Size1-Regular.woff2",
-    "scripts/katex/fonts/KaTeX_Size2-Regular.woff2",
-    "scripts/katex/fonts/KaTeX_Size3-Regular.woff2",
-    "scripts/katex/fonts/KaTeX_Size4-Regular.woff2",
-    "scripts/katex/fonts/KaTeX_Typewriter-Regular.woff2",
-    "scripts/vexflow/vexflow-min.js",
-    "scripts/vexflow/vexflow-min.css",
-    "scripts/vexflow/auto-render.min.js"*/
 ];
 
 const notesCache = [
